4b8744ea4e97fdf094ab1e546f996adab9a17d07
cilou <cilou.infoiepscf@gmail.com>
2018-10-03 14:24:35 +0200

    Initial commit -- Development environment initialization

---
+++ src/main/java/servlets/Partition.java
@@ -0,0 +1,38 @@
+package servlets;
+
+import com.google.gson.JsonObject;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Partition extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        JsonObject jsonObj = new JsonObject();
+        jsonObj.addProperty("code", 200);
+        jsonObj.addProperty("message", "Test OK");
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(200);
+        response.getWriter().write(jsonObj.toString());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebServices</title>
+</head>
+<body>
+    <h1>MusicPlay Web Services</h1>
+    <p>GET /partitions => <a href="/partitions">Servlet Partition</a></p>
+</body>
+</html>
