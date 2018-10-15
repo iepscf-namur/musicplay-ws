ba27baf1bc1602bd5e269c2e9c82e0700dfa3f64
Cilou <cilou.infoiepscf@gmail.com>
2018-10-15 19:54:00 +0200

    Password DB updated in DAOFactory

--- src/main/java/DAO/DAOFactory.java
+++ src/main/java/DAO/DAOFactory.java
@@ -24,7 +24,7 @@ public class DAOFactory {
 
         }
 
-        DAOFactory instance = new DAOFactory("jdbc:mysql://localhost:3306/musicplaydb", "root", "");
+        DAOFactory instance = new DAOFactory("jdbc:mysql://localhost:3306/musicplaydb", "root", "1234");
         return instance;
     }
 
